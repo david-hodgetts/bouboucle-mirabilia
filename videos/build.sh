13282df6ac3c9628134d0ede7850845b099fabeb
David Hodgetts <david.demainlalune@gmail.com>
2023-09-26 21:28:40 +0200

    wip

--- videos/build.sh
+++ videos/build.sh
@@ -6,16 +6,16 @@ declare -a arr=("arc-en-ciel" "chenille-clignotante" "etoile" "explosion" "rythm
 for filename in "${arr[@]}"
 do
     source="${filename}.mkv"
-    cropped="${filename}.cropped.ts";
+    cropped="${filename}.cropped.mp4";
     # original res = 725 x 532
     # cropped res = 725 x 462
     ffmpeg -y -i "$source" -filter:v crop=752:462:0:70 "$cropped"
     
     # generate titles
-    titlefilename="${filename}.title.ts"
-    ffmpeg -y -f lavfi -i color=size=752x462:duration=5:rate=30:color=black -vf "drawtext=fontfile=arial.ttf:fontsize=80:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text=$filename" "${titlefilename}"
+    title="${filename}.title.mp4"
+    ffmpeg -y -f lavfi -i color=size=752x462:duration=5:rate=30:color=black -vf "drawtext=fontfile=arial.ttf:fontsize=80:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text=$filename" "${title}"
 
-    finalname="${filename}.webm"
+    final="${filename}.mp4"
 
-    ffmpeg -y -i "concat:$titlefilename|$cropped" -c copy "${filename}.concat.mp4"
+    ffmpeg -y -i $title -i $cropped -filter_complex "[0:v:0][0:a:0][1:v:0][1:a:0]concat=n=2:v=1:a=1[outv][outa]" -map "[outv]" -map "[outa]" $final
 done
