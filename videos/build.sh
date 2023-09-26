694fe72bb53c0d7c2454b4c19f069d2b0e16be4c
David Hodgetts <david.demainlalune@gmail.com>
2023-09-26 21:34:05 +0200

    improves video conversion script

--- videos/build.sh
+++ videos/build.sh
@@ -15,7 +15,11 @@ do
     title="${filename}.title.mp4"
     ffmpeg -y -f lavfi -i color=size=752x462:duration=5:rate=30:color=black -vf "drawtext=fontfile=arial.ttf:fontsize=80:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text=$filename" "${title}"
 
-    final="${filename}.mp4"
 
-    ffmpeg -y -i $title -i $cropped -filter_complex "[0:v:0][0:a:0][1:v:0][1:a:0]concat=n=2:v=1:a=1[outv][outa]" -map "[outv]" -map "[outa]" $final
+    # concat title and cropped video
+    final="${filename}.webm"
+    ffmpeg -y -i $title -i $cropped -filter_complex "[0:v:0][1:v:0]concat=n=2:v=1:a=0[outv]" -map "[outv]" $final
+    
+    # move to destination
+    mv $final ../public/videos/$final
 done
